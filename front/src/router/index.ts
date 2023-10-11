19e02e4ac3b5cf0fcf69033996c48634706e20c9
Bartosz Panek <m.panekbartosz@gmail.com>
2023-10-11 21:33:18 +0200

    Create MainDashboard component
    + add route

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -3,12 +3,14 @@ import Home from '../components/Home.vue'
 import AboutUs from '../components/AboutUs.vue'
 import LoginPage from '../components/LoginPage.vue'
 import SignupPage from '../components/SignupPage.vue'
+import MainDashboard from '../components/MainDashboard.vue'
 
 const routes: RouteRecordRaw[] = [
     { path: '/', name:"Home", component: Home },
     { path: '/about',name:"About", component: AboutUs },
     { path: '/login',name:"Login", component: LoginPage },
     { path: '/signup',name:"SignUp", component: SignupPage },
+    { path: '/dashboard',name:"Dashboard", component: MainDashboard },
   ]
   
   const router = createRouter({
