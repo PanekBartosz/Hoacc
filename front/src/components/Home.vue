c2b0a08c7a910f93b6a58e92fdc446a6ea665e69
Bartosz Panek <m.panekbartosz@gmail.com>
2023-10-12 16:19:30 +0200

    run prettier

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -1,39 +1,41 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import TypedText from './TypedText.vue'
-import { useRouter } from 'vue-router'
+import { ref } from "vue";
+import TypedText from "./TypedText.vue";
+import { useRouter } from "vue-router";
 
-const router = useRouter()
-const localStorageKey = 'isVisibleState' 
+const router = useRouter();
+const localStorageKey = "isVisibleState";
 
 const navigateToSignup = () => {
   localStorage.setItem(localStorageKey, JSON.stringify(false));
-  router.push('/signup');
-}
-
+  router.push("/signup");
+};
 </script>
 <template>
-    <div class="text-center font-bold flex h-[80vh] justify-center">
-      <div class="m-auto">
-        <p class="text-2xl sm:text-3xl text-transparent font-outline-2">
-          HOME ACCOUNTING
-        </p>
-        <p class="text-font-white text-2xl sm:text-6xl">
-          Efficient use of the budget.
-        </p>
-        <div class="text-font-black text-2xl sm:text-5xl m-2 flex items-center justify-center">
-          <TypedText />
-          <span class="ml-2">
-            accounting
-          </span>
-        </div>
-        <p class="text-font-grey text-xl sm:text-3xl">
-          Monitor spending data to increase efficiency in household budget management.
-        </p>
-        <button @click="navigateToSignup" class="px-5 py-1 sm:px-10 sm:py-5 rounded-lg mt-4 mb-5 text-lg sm:text-2xl shadow-custom bg-white 
-          text-font-black hover:bg-black hover:text-white transition duration-300 ease-in-out">
-          GET STARTED
-        </button>
+  <div class="text-center font-bold flex h-[80vh] justify-center">
+    <div class="m-auto">
+      <p class="text-2xl sm:text-3xl text-transparent font-outline-2">
+        HOME ACCOUNTING
+      </p>
+      <p class="text-font-white text-2xl sm:text-6xl">
+        Efficient use of the budget.
+      </p>
+      <div
+        class="text-font-black text-2xl sm:text-5xl m-2 flex items-center justify-center"
+      >
+        <TypedText />
+        <span class="ml-2"> accounting </span>
       </div>
+      <p class="text-font-grey text-xl sm:text-3xl">
+        Monitor spending data to increase efficiency in household budget
+        management.
+      </p>
+      <button
+        @click="navigateToSignup"
+        class="px-5 py-1 sm:px-10 sm:py-5 rounded-lg mt-4 mb-5 text-lg sm:text-2xl shadow-custom bg-white text-font-black hover:bg-black hover:text-white transition duration-300 ease-in-out"
+      >
+        GET STARTED
+      </button>
     </div>
+  </div>
 </template>
--- front/src/components/StartingPage.vue
+++ front/src/components/StartingPage.vue
@@ -1,62 +1,75 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
-import Logo from './Logo.vue'
-import { useRouter } from 'vue-router'
+import { computed, ref, watch } from "vue";
+import Logo from "./Logo.vue";
+import { useRouter } from "vue-router";
 
-const router = useRouter()
+const router = useRouter();
 
-const localStorageKey = 'isVisibleState' // Key for storing in localStorage
-let initialIsVisible = localStorage.getItem(localStorageKey)
-const isVisible = ref(true)
+const localStorageKey = "isVisibleState"; // Key for storing in localStorage
+let initialIsVisible = localStorage.getItem(localStorageKey);
+const isVisible = ref(true);
 
 const navigateToHome = () => {
-  isVisible.value = true
-  localStorage.setItem(localStorageKey, JSON.stringify(true))
-  router.push({ name: 'Home' })
-  router.push('/')
-}
+  isVisible.value = true;
+  localStorage.setItem(localStorageKey, JSON.stringify(true));
+  router.push({ name: "Home" });
+  router.push("/");
+};
 
 const navigateToAbout = () => {
-  router.push({ name: 'About' })
-  router.push('/about')
-}
+  router.push({ name: "About" });
+  router.push("/about");
+};
 const navigateToLogin = () => {
-  isVisible.value = false
-  localStorage.setItem(localStorageKey, JSON.stringify(false))
-  router.push('/login')
-}
+  isVisible.value = false;
+  localStorage.setItem(localStorageKey, JSON.stringify(false));
+  router.push("/login");
+};
 
-watch(() => router.currentRoute.value, () => {
-  initialIsVisible = localStorage.getItem(localStorageKey)
-  isVisible.value = initialIsVisible !== null ? JSON.parse(initialIsVisible) : true
-})
+watch(
+  () => router.currentRoute.value,
+  () => {
+    initialIsVisible = localStorage.getItem(localStorageKey);
+    isVisible.value =
+      initialIsVisible !== null ? JSON.parse(initialIsVisible) : true;
+  }
+);
 
 const isActiveRoute = (routeName) => {
   return router.currentRoute.value.name === routeName;
-}
+};
 
 const isDashboardRoute = computed(() => {
-  return router.currentRoute.value.path === '/dashboard';
-})
-
+  return router.currentRoute.value.path === "/dashboard";
+});
 </script>
 
 <template>
   <div class="p-5">
-    <Logo v-if="!isDashboardRoute" @click="navigateToHome"/>
+    <Logo v-if="!isDashboardRoute" @click="navigateToHome" />
     <div v-if="isVisible && !isDashboardRoute">
       <div class="text-right xxs:mt-5 flex justify-end">
         <div class="flex items-center md:mr-[50px]">
-          <button @click="navigateToHome" :class="{ 'text-white': isActiveRoute('Home') }" class="text-[16px] mt-7 xxs:mt-0 sm:mr-6 mr-3 font-bold sm:text-[32px]">
+          <button
+            @click="navigateToHome"
+            :class="{ 'text-white': isActiveRoute('Home') }"
+            class="text-[16px] mt-7 xxs:mt-0 sm:mr-6 mr-3 font-bold sm:text-[32px]"
+          >
             HOME
           </button>
-          <button @click="navigateToAbout" :class="{ 'text-white': isActiveRoute('About') }" class="text-[16px] mt-7 xxs:mt-0 sm:mr-6 mr-3 font-bold sm:text-[32px]">
+          <button
+            @click="navigateToAbout"
+            :class="{ 'text-white': isActiveRoute('About') }"
+            class="text-[16px] mt-7 xxs:mt-0 sm:mr-6 mr-3 font-bold sm:text-[32px]"
+          >
             ABOUT US
           </button>
-          <button @click="navigateToLogin"  class="px-2 py-0 mt-7 xxs:mt-0 sm:px-4 sm:py-1 font-semibold rounded-lg text-lg shadow-custom bg-white 
-              text-font-black hover:bg-black hover:text-white transition duration-300 ease-in-out">
+          <button
+            @click="navigateToLogin"
+            class="px-2 py-0 mt-7 xxs:mt-0 sm:px-4 sm:py-1 font-semibold rounded-lg text-lg shadow-custom bg-white text-font-black hover:bg-black hover:text-white transition duration-300 ease-in-out"
+          >
             LOG IN
-          </button>      
+          </button>
         </div>
       </div>
     </div>
