4652e9a3af972892a8095a83d0e77d3cf234199e
Bartosz Panek <m.panekbartosz@gmail.com>
2023-10-16 15:21:08 +0200

    Implement operations in dashboard

--- front/src/components/MainDashboard.vue
+++ front/src/components/MainDashboard.vue
@@ -1,5 +1,30 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import MainNavbar from "./MainNavbar.vue";
+
+const currentDate = new Date()
+
+const formattedDate = new Intl.DateTimeFormat('en-US', {
+  day: '2-digit',
+  month: 'long',
+  year: 'numeric'
+}).format(currentDate)
+
+interface Operation {
+  date: string
+  category: string
+  description: string
+  amount: number
+}
+
+const testOperation: Operation = {
+  date: formattedDate,
+  category: 'EDUCATION',
+  description: 'New book - c# in depth',
+  amount: 53.01
+}
+
+const operations = ref<Operation[]>([...Array(10).keys()].map(() => testOperation))
 </script>
 <template>
   <MainNavbar />
@@ -40,8 +65,85 @@ import MainNavbar from "./MainNavbar.vue";
     </div>
 
     <!-- Container 2 -->
-    <div class="w-full h-full bg-white rounded-lg shadow-lg p-4 mb-5">
-      Container 2
+    <div class="w-full h-full overflow-hidden bg-white rounded-lg shadow-lg p-4 mb-5">
+      <h3 class="text-xl font-medium text-gray-700">Operations</h3>
+      <div class="flex flex-col mt-8">
+      <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+        <div class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
+          <table class="min-w-full">
+            <thead>
+              <tr>
+                <th
+                  class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50"
+                >
+                  Date
+                </th>
+                <th
+                  class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50"
+                >
+                  Category
+                </th>
+                <th
+                  class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50"
+                >
+                  Description
+                </th>
+                <th
+                  class="px-6 py-3 text-xs font-medium leading-4 tracking-wider text-left text-gray-500 uppercase border-b border-gray-200 bg-gray-50"
+                >
+                  Amount
+                </th>
+                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50" />
+              </tr>
+            </thead>
+
+            <tbody class="bg-white">
+              <tr v-for="(o, index) in operations" :key="index">
+                <td
+                  class="px-6 py-4 border-b border-gray-200 whitespace-nowrap"
+                >
+                  <div class="flex items-center">
+                    <div class="ml-4">
+                      <div class="text-sm font-medium leading-5 text-gray-900">
+                        {{ o.date }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+
+                <td
+                  class="px-6 py-4 border-b border-gray-200 whitespace-nowrap"
+                >
+                  <div class="text-sm leading-5 text-gray-900">
+                    {{ o.description }}
+                  </div>
+                </td>
+
+                <td
+                  class="px-6 py-4 border-b border-gray-200 whitespace-nowrap"
+                >
+                  <span
+                    class="inline-flex px-2 text-xs font-semibold leading-5 text-blue-800 bg-blue-200 rounded-full"
+                  >{{ o.category }}</span>
+                </td>
+
+                <td
+                  class="px-6 py-4 text-sm leading-5 text-gray-900 border-b border-gray-200 whitespace-nowrap"
+                >
+                  {{ o.amount }}
+                </td>
+
+                <td
+                  class="px-6 py-4 text-sm font-bold leading-5 text-right border-b border-gray-200 whitespace-nowrap"
+                >
+                  <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
     </div>
 
     <!-- Container 3 & 4  -->
