c0f9d969253e2c65a11368b45b5421a466ae8347
Bartosz Panek <m.panekbartosz@gmail.com>
2023-11-02 20:39:38 +0100

    Update UserRepository

--- HoaccAPI/Hoacc.Data.Sql/User/UserRepository.cs
+++ HoaccAPI/Hoacc.Data.Sql/User/UserRepository.cs
@@ -1,6 +1,55 @@
-﻿namespace HoaccDataSql.User;
+﻿using System.Threading.Tasks;
+using HoaccIData.User;
+using Google.Protobuf.WellKnownTypes;
+using Microsoft.EntityFrameworkCore;
 
-public class UserRepository
+namespace HoaccDataSql.User
 {
-    
+    public class UserRepository: IUserRepository
+    {
+        private readonly HoaccDbContext _context;
+        private IUserRepository _userRepositoryImplementation;
+
+        public UserRepository(HoaccDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<HoaccDomain.User.User> GetUser(string email)
+        {
+            var user = await _context.User.FirstOrDefaultAsync(x=>x.Email == email);
+            return new HoaccDomain.User.User(user.UserId,
+                user.Email,
+                user.Password);
+        }
+        
+        public async Task<HoaccDomain.User.User> GetUser(int userId)
+        {
+            var user = await _context.User.FirstOrDefaultAsync(x=>x.UserId == userId);
+            return new HoaccDomain.User.User(user.UserId,
+                user.Email,
+                user.Password);
+        }
+        
+        public async Task<int> CreateUser(HoaccDomain.User.User user)
+        {
+            var userDAO =  new DAO.User { 
+                UserId = user.UserId,
+                Email = user.Email,
+                Password = user.Password
+            };
+            await _context.AddAsync(userDAO);
+            await _context.SaveChangesAsync();
+            return userDAO.UserId;
+        }
+
+        public async Task UpdateUserPassword(int userId, HoaccDomain.User.User user)
+        {
+            var updateUserPassword = await _context.User.FirstOrDefaultAsync(x=>x.UserId == user.UserId);
+            updateUserPassword.Password = user.Password;
+            await _context.SaveChangesAsync();
+        }
+
+    }
+
 }
