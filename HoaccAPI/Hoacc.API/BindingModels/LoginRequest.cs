d49a9ec014f013bb59fae01ca0c6b6b81311e58f
Bartosz Panek <m.panekbartosz@gmail.com>
2023-11-14 19:58:14 +0100

    Implement AuthenticationController

---
+++ HoaccAPI/Hoacc.API/BindingModels/LoginRequest.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Build.Framework;
+
+namespace HoaccAPI.BindingModels;
+
+public class LoginRequest
+{
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
---
+++ HoaccAPI/Hoacc.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HoaccAPI.BindingModels;
+using HoaccDataSql;
+
+[ApiController]
+[Route("api/auth")]
+public class AuthenticationController : ControllerBase
+{
+    private readonly HoaccDbContext _context;
+
+    public AuthenticationController(HoaccDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest("Invalid request");
+        }
+
+        var user = await _context.User
+            .FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid email or password");
+        }
+        return Ok(new { user.UserId});
+    }
+}
